29b45637dd0a3b63282b3cd2dd0d8fe849d64e20
YuZakuro <zakuro@yuzakuro.me>
2015-09-28 19:48:01 +0900

    Add binary_search

---
+++ algorithm/binary_search.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+T binary_search(function<bool(T)> func, T include, T exclude)
+{
+  while (abs(exclude - include) > 1)
+  {
+    auto middle = (include + exclude) / 2;
+    (func(middle) ? include : exclude) = middle;
+  }
+  return include;
+}
